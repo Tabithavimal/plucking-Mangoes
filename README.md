46bf1b5e82dec5bacb9ac926f6e2ef1aec104bff
Tabithavimal <73104714+Tabithavimal@users.noreply.github.com>
2020-11-25 09:13:27 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# plucking-Mangoes
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ sketch.js
@@ -0,0 +1,42 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+boy1=new Boy(150,620,40,40)
+
+tree1=new tree(600,600,100,100)
+
+
+ground=new Ground(width/2,670,width,20)
+	Engine.run(engine);
+	
+
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+boy1.display();
+ground.display();
+}
+
+
+
